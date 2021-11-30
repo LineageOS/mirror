497bc17b2b5dba0e00953c5674fa9149710a4bd0
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-30 19:39:52 +0530

    aosp-minimal: Include kernel/manifest, not synced by default
    Change-Id: I925953b9eaa2d05b11ef9069de214cf82daa559c

--- aosp-minimal-regen.py
+++ aosp-minimal-regen.py
@@ -5,8 +5,6 @@ import re
 import sys
 import xml.etree.ElementTree as ET
 
-PLATFORM_MANIFEST = "platform/manifest"
-
 def groups_for_repo(repo):
     groups = set()
 
@@ -51,15 +49,25 @@ else:
     print("Downloading aosp_manifest repository...")
     aosp_manifest = git.Repo.clone_from("https://android.googlesource.com/platform/manifest", "aosp_manifest")
 
-# Get all the refs
-refs = [tag for tag in sorted(aosp_manifest.git.tag(l=True).splitlines())]  # All the tags...
-refs.append('origin/master')  # ...and master
+if os.path.isdir("aosp_kernel_manifest"):
+    print("Updating kernel_manifest repository...")
+    kernel_manifest = git.Repo("aosp_kernel_manifest")
+    kernel_manifest.remote("origin").fetch()
+else:
+    print("Downloading aosp_kernel_manifest repository...")
+    kernel_manifest = git.Repo.clone_from("https://android.googlesource.com/kernel/manifest", "aosp_kernel_manifest")
 
-repos = set()
+# Get all the refs
+aosp_refs = [tag for tag in sorted(aosp_manifest.git.tag(l=True).splitlines())]  # All the tags...
+aosp_refs.append('origin/master')  # ...and master
+kernel_refs = [h.name for h in kernel_manifest.refs]  # All the refs, repo only has branches no tags...
+
+aosp_repos = set()
+kernel_repos = set()
 
 # Look for repo names in each ref
-for index, ref in enumerate(refs, 1):
-    print("[{}/{}] Parsing `{}`...".format(index, len(refs), ref))
+for index, ref in enumerate(aosp_refs, 1):
+    print("[{}/{}] Parsing `{}`...".format(index, len(aosp_refs), ref))
 
     # Load the XML
     manifest_xml = ET.fromstring(aosp_manifest.git.show("{}:default.xml".format(ref)))
@@ -69,8 +77,31 @@ for index, ref in enumerate(refs, 1):
         if child.tag != "project":
             continue
 
-        repos.add(child.attrib["name"])
+        aosp_repos.add(child.attrib["name"])
 
+for index, ref in enumerate(kernel_refs, 1):
+    # Skip broken refs
+    if ref == "origin/android-gs-raviole-mainline":
+        continue
+    print("[{}/{}] Parsing `{}`...".format(index, len(kernel_refs), ref))
+
+    # Load the XML
+    manifest_xml = ET.fromstring(kernel_manifest.git.show("{}:default.xml".format(ref)))
+
+    for child in manifest_xml:
+        # Skip all non-project tags
+        if child.tag != "project":
+            continue
+
+        kernel_repos.add(child.attrib["name"])
+
+
+# Always add the manifest so one can sync from this mirror
+aosp_repos.add("platform/manifest")
+kernel_repos.add("kernel/manifest")
+
+# Remove duplicate repos
+kernel_repos = kernel_repos - aosp_repos
 
 file = open("aosp-minimal.xml", "w")
 file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
@@ -83,11 +114,7 @@ file.write("           remote=\"aosp\"\n")
 file.write("           sync-j=\"4\" />\n")
 file.write("\n")
 
-# Always add platform/manifest so one can sync from this manifest
-if PLATFORM_MANIFEST not in repos:
-    repos.add(PLATFORM_MANIFEST)
-
-for repo in sorted(repos):
+for repo in sorted(aosp_repos):
     # remove an unavailable repository
     if repo == "platform/packages/apps/OMA-DM":
         continue
@@ -95,13 +122,27 @@ for repo in sorted(repos):
     line = "name=\"" + repo + "\""
 
     # Would we get a path conflict?
-    if any(s.startswith(repo + "/") for s in repos):
+    if any(s.startswith(repo + "/") for s in aosp_repos):
         line += " path=\"" + repo + ".git\""
 
     # Add groups
     groups = groups_for_repo(repo)
     if len(groups) > 0:
         line += " groups=\"" + ",".join(groups) + "\""
+
+    file.write("  <project " + line + " />\n")
+
+file.write("  <!-- kernel/manifest -->\n")
+
+for repo in sorted(kernel_repos):
+    line = "name=\"" + repo + "\""
+
+    # Would we get a path conflict?
+    if any(s.startswith(repo + "/") for s in kernel_repos):
+        line += " path=\"" + repo + ".git\""
+
+    # Add groups
+    line += " groups=\"kernel,notdefault\""
 
     file.write("  <project " + line + " />\n")
 
